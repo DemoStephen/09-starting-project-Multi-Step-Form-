19a290304d64c5fc494279dd0760f50298c1a1ad
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-14 02:09:03 +0100

    Created sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import SideBarItem from "./SideBarItem";
+
+export default function SideBar() {
+  return (
+    <>
+      <nav>
+        <SideBarItem title="Your info" step="1" />
+        <SideBarItem title="Select plan" step="2" />
+        <SideBarItem title="Add-ons" step="3" />
+        <SideBarItem title="Summary" step="4" />
+      </nav>
+    </>
+  );
+}
