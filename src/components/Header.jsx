ebbb616a9694c715035a4346a6e3d5ab30890af7
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-14 02:03:29 +0100

    created and modified Header components

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <>
+      <header>
+        <h1>Personal info</h1>
+        <p>Please provide your name, email address, and phone number.</p>
+      </header>
+    </>
+  );
+}
