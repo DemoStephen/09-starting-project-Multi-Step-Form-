35898a146d16d0d0ed4749aa8dc0f27cc9216783
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-14 02:09:28 +0100

    Thank you state created

---
+++ src/components/ThankYou.jsx
@@ -0,0 +1,15 @@
+export default function ThankYou() {
+  return (
+    <>
+      <section className="sectionFive d-flex d-none">
+        <img src="./assets/images/icon-thank-you.svg" alt="thank you" />
+        <h1>Thank you!</h1>
+        <p>
+          Thanks htmlFor confirming your subscription! We hope you have fun
+          using our plathtmlForm. If you ever need support, please feel free to
+          email us at adewalestephennifemi@gmail.com.
+        </p>
+      </section>
+    </>
+  );
+}
