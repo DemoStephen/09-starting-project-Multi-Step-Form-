0c7d14e8a5bbe5e19be13246a2f53ac0f7115bb7
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-15 11:38:57 +0100

    update Thank you state

--- src/components/ThankYou.jsx
+++ src/components/ThankYou.jsx
@@ -1,13 +1,14 @@
+import thankYou from "../assets/images/icon-thank-you.svg";
 export default function ThankYou() {
   return (
     <>
-      <section className="sectionFive d-flex d-none">
-        <img src="./assets/images/icon-thank-you.svg" alt="thank you" />
+      <section className="sectionFive d-flex">
+        <img src={thankYou} alt="thank you" />
         <h1>Thank you!</h1>
         <p>
-          Thanks htmlFor confirming your subscription! We hope you have fun
-          using our plathtmlForm. If you ever need support, please feel free to
-          email us at adewalestephennifemi@gmail.com.
+          Thanks for confirming your subscription! We hope you have fun using
+          our platform. If you ever need support, please feel free to email us
+          at adewalestephennifemi@gmail.com.
         </p>
       </section>
     </>
