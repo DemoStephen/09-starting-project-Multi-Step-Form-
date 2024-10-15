c04bd4f4a0c2cbf06705dc3e04a7c915dce356dd
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-15 11:30:55 +0100

    Created util that contains data that is meant to be in the heading component

---
+++ src/util/data.js
@@ -0,0 +1,18 @@
+export const header = [
+  {
+    heading: "Personal info",
+    text: "Please provide your name, email address, and phone number.",
+  },
+  {
+    heading: "Select your plan",
+    text: "You have the option of monthly or yearly billing.",
+  },
+  {
+    heading: "Pick add-ons",
+    text: "Add-ons help enhance your gaming experience.",
+  },
+  {
+    heading: "Finishing up",
+    text: "Double-check everything looks OK before confirming.",
+  },
+];
