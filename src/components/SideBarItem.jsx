2ffa59346a580be01a6fb764a123f75641bf70e1
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-15 11:43:33 +0100

    added class to the sidebar Item so clients could know what section of the form they are filling

--- src/components/SideBarItem.jsx
+++ src/components/SideBarItem.jsx
@@ -1,9 +1,15 @@
 /* eslint-disable react/prop-types */
-export default function SideBarItem({ title, step }) {
+export default function SideBarItem({ title, step, currentView }) {
   return (
     <>
       <section className="d-flex">
-        <div className="navIndex d-flex">{step}</div>
+        <div
+          className={`navIndex d-flex ${
+            currentView === step - 1 && "currentForm"
+          }`}
+        >
+          {step}
+        </div>
         <div>
           <span>Step {step}</span>
           <p>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,14 @@
+/* eslint-disable react/prop-types */
 import SideBarItem from "./SideBarItem";
 
-export default function SideBar() {
+export default function SideBar({ currentView }) {
   return (
     <>
       <nav>
-        <SideBarItem title="Your info" step="1" />
-        <SideBarItem title="Select plan" step="2" />
-        <SideBarItem title="Add-ons" step="3" />
-        <SideBarItem title="Summary" step="4" />
+        <SideBarItem title="Your info" step={1} currentView={currentView} />
+        <SideBarItem title="Select plan" step={2} currentView={currentView} />
+        <SideBarItem title="Add-ons" step={3} currentView={currentView} />
+        <SideBarItem title="Summary" step={4} currentView={currentView} />
       </nav>
     </>
   );
