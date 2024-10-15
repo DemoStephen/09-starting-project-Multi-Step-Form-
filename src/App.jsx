6172eee836c487f16449237b795b8949b59124b1
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-15 11:45:51 +0100

    added some conditional statement so the right information is being displayed to the client

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,30 @@ import "./App.css";
 import Form from "./components/Form";
 import SideBar from "./components/Sidebar";
 import ThankYou from "./components/ThankYou";
+import { useState } from "react";
 export default function App() {
+  const [currentView, setCurrentView] = useState(0);
+  const [submit, setSubmit] = useState(false);
+  function handleNextView() {
+    setCurrentView((prev) => (prev = prev + 1));
+  }
+  function handleSubmit() {
+    setSubmit(true);
+  }
   return (
     <>
       <main className="d-flex">
-        <SideBar />
+        <SideBar currentView={currentView} />
         <section className="section">
-          <Form />
-          <ThankYou />
+          {submit ? (
+            <ThankYou />
+          ) : (
+            <Form
+              currentView={currentView}
+              onChangeView={() => handleNextView()}
+              onSubmit={handleSubmit}
+            />
+          )}
         </section>
       </main>
     </>
