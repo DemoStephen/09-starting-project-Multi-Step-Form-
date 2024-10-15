6d54a63ed3192cd3e253e6e4fefd93d3fb9e32a9
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-15 11:47:35 +0100

    added conditional statement to display the right information to the user

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,26 +1,40 @@
+/* eslint-disable react/prop-types */
 import FormSection1 from "./FormSection1";
 import FormSection2 from "./FormSection2";
 import FormSection3 from "./FormSection3";
 import FormSection4 from "./FormSection4";
 import Header from "./Header";
-
-export default function Form() {
+import { header } from "../util/data";
+export default function Form({ currentView, onChangeView, onSubmit }) {
   return (
     <>
       <form action="#" method="post">
-        <Header />
-        <FormSection1 />
-        <FormSection2 />
-        <FormSection3 />
-        <FormSection4 />
+        <Header
+          title={header[currentView].heading}
+          paragraph={header[currentView].text}
+        />
+        {currentView === 0 && <FormSection1 />}
+        {currentView === 1 && <FormSection2 />}
+        {currentView === 2 && <FormSection3 />}
+        {currentView === 3 && <FormSection4 />}
 
         <section className=" nextPrevious">
           <div className="d-flex">
-            <button type="button">Go Back</button>
-            <button type="button">Next Step</button>
-            <button type="submit" className="d-none">
-              Submit
-            </button>
+            {currentView > 0 && (
+              <button type="button" className="goBack">
+                Go Back
+              </button>
+            )}
+            {currentView < 3 && (
+              <button type="button" onClick={() => onChangeView()}>
+                Next Step
+              </button>
+            )}
+            {currentView > 2 && (
+              <button type="submit" onClick={() => onSubmit()}>
+                Submit
+              </button>
+            )}
           </div>
           <div className="attribution">
             Challenge by{" "}
