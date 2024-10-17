2a21baf84b5faf40d2e2c96e6820a1bbd192809d
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-18 00:56:11 +0100

    Updated the form components

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,11 +5,18 @@ import FormSection3 from "./FormSection3";
 import FormSection4 from "./FormSection4";
 import Header from "./Header";
 import { header } from "../util/data";
+import SectionChange from "./SectionChangeBtn";
 export default function Form({
   currentView,
   onNextView,
   onPreviousView,
   onSubmit,
+  name,
+  email,
+  phone,
+  nameError,
+  emailError,
+  phoneError,
 }) {
   return (
     <>
@@ -18,39 +25,25 @@ export default function Form({
           title={header[currentView].heading}
           paragraph={header[currentView].text}
         />
-        <FormSection1 currentView={currentView} />
+
+        <FormSection1
+          currentView={currentView}
+          name={name}
+          email={email}
+          phone={phone}
+          nameError={nameError}
+          emailError={emailError}
+          phoneError={phoneError}
+        />
         <FormSection2 currentView={currentView} />
         <FormSection3 currentView={currentView} />
         <FormSection4 currentView={currentView} />
 
-        <section className=" nextPrevious">
-          <div className="d-flex">
-            <button
-              type="button"
-              className={`goBack ${currentView > 0 ? "" : "visibility"}`}
-              onClick={() => onPreviousView()}
-            >
-              Go Back
-            </button>
-            {currentView < 3 ? (
-              <button type="button" onClick={() => onNextView()}>
-                Next Step
-              </button>
-            ) : (
-              <button type="submit">Submit</button>
-            )}
-          </div>
-          <div className="attribution">
-            Challenge by{" "}
-            <a
-              href="https://www.frontendmentor.io?ref=challenge"
-              target="_blank"
-            >
-              Frontend Mentor
-            </a>
-            . Coded by <a href="#">Adewale Stephen</a>.
-          </div>
-        </section>
+        <SectionChange
+          onPreviousView={onPreviousView}
+          onNextView={onNextView}
+          currentView={currentView}
+        />
       </form>
     </>
   );
