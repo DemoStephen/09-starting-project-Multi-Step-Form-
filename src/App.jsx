6ad155f310a5d92ba49a8579ed589f2f6dc246ca
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-18 00:58:27 +0100

    validate section one of the form. Also added error messaage when user gives the wrong info

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
+/* eslint-disable no-useless-escape */
 import "./App.css";
 import Form from "./components/Form";
 import SideBar from "./components/Sidebar";
 import ThankYou from "./components/ThankYou";
-import { useState } from "react";
+import { useState, useRef } from "react";
+
 export default function App() {
   const [currentView, setCurrentView] = useState(0);
   const [submit, setSubmit] = useState(false);
+
+  const name = useRef();
+  const email = useRef();
+  const phone = useRef();
+
+  const [nameError, setNameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [phoneError, setPhoneError] = useState("");
+
+  function formValidation() {
+    const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    const data = {
+      name: name.current.value.trim(),
+      email: email.current.value.trim(),
+      phone: phone.current.value.trim(),
+    };
+    {
+      !data.name ? setNameError("Name is required") : setNameError("");
+    }
+    {
+      !data.phone ? setPhoneError("Phone number required") : setPhoneError("");
+    }
+    {
+      data.email.match(mailformat)
+        ? setEmailError("")
+        : setEmailError("Valid Email Is required");
+    }
+
+    if (data.name && data.phone && data.email.match(mailformat)) {
+      setCurrentView((prev) => prev + 1);
+    } else {
+      setCurrentView(0);
+    }
+    return;
+  }
   function handleNextView() {
+    if (currentView === 0) {
+      return formValidation();
+    }
     setCurrentView((prev) => prev + 1);
   }
   function handlePreviousView() {
@@ -15,6 +56,7 @@ export default function App() {
   function handleSubmit() {
     setSubmit(true);
   }
+
   return (
     <>
       <main className="d-flex">
@@ -28,6 +70,12 @@ export default function App() {
               onNextView={() => handleNextView()}
               onPreviousView={() => handlePreviousView()}
               onSubmit={handleSubmit}
+              name={name}
+              email={email}
+              phone={phone}
+              nameError={nameError}
+              emailError={emailError}
+              phoneError={phoneError}
             />
           )}
         </section>
