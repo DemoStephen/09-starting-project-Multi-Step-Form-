6427b5d061cc9c286c7738710c2f6afea5cf6855
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-19 15:25:38 +0100

    included the map method to further make my code clear and understandable

--- src/components/FormSection2.jsx
+++ src/components/FormSection2.jsx
@@ -1,39 +1,53 @@
 /* eslint-disable react/prop-types */
 import InputRadio from "./InputRadio";
-import arcade from "../assets/images/icon-arcade.svg";
-import advanced from "../assets/images/icon-advanced.svg";
-import pro from "../assets/images/icon-pro.svg";
-export default function FormSection2({ currentView }) {
+import { forwardRef } from "react";
+import { Plans } from "../util/data";
+const FormSection2 = forwardRef(function FormSection2(
+  { currentView, plans, handlePlan, plan, checked, onChange },
+  ref
+) {
   return (
     <>
       <section
         className={`sectionTwo d-flex ${currentView === 1 ? "" : "d-none"}`}
       >
         <section className="d-flex">
-          <InputRadio id="arcade" imgSrc={arcade} title="Arcade" price="9/mo" />
-          <InputRadio
-            id="advanced"
-            imgSrc={advanced}
-            title="Advanced"
-            price="12/mo"
-            Checked={true}
-          />
-          <InputRadio id="pro" imgSrc={pro} title="Pro" price="15/mo" />
+          {Plans.map((info) => {
+            return (
+              <InputRadio
+                key={info.plan}
+                imgSrc={info.image}
+                title={info.plan}
+                price={`${info.monthly}/mo`}
+                ref={plans}
+                onChange={handlePlan}
+                selectedPlan={plan}
+              />
+            );
+          })}
         </section>
         <div className="planTimeline d-flex">
-          <p className="yearlyOrMonthly">Monthly</p>
+          <p className={`${checked === "monthly" && "yearlyOrMonthly"}`}>
+            Monthly
+          </p>
           <div className="toggleContainer d-flex">
             <input
               type="checkbox"
               name="planOption"
               id="planOption"
               className="d-none"
+              value={checked}
+              ref={ref}
+              onChange={onChange}
             />
             <label htmlFor="planOption" className="toggle"></label>
           </div>
-          <p>Yearly</p>
+          <p className={`${checked === "yearly" && "yearlyOrMonthly"}`}>
+            Yearly
+          </p>
         </div>
       </section>
     </>
   );
-}
+});
+export default FormSection2;
