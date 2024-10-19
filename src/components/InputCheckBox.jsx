eea901dcb98e382f6842effb2fe931a16b969287
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-19 19:49:48 +0100

    still fixing bug, Soon enough it would be fixed

--- src/components/InputCheckBox.jsx
+++ src/components/InputCheckBox.jsx
@@ -1,15 +1,35 @@
+import { forwardRef } from "react";
+
 /* eslint-disable react/prop-types */
-export default function InputCheckBox({ id, title, about, price }) {
+const InputCheckBox = forwardRef(function InputCheckBox(
+  { id, title, about, price, onChange, duration },
+  ref
+) {
+  const data = {
+    name: { title },
+    price: { price },
+    abbr: "mo",
+    id: { id },
+  };
   return (
     <label htmlFor={id} className="d-flex">
       <div className="d-flex gap">
-        <input type="checkbox" name="addOns" id={id} />
+        <input
+          type="checkbox"
+          name="addOns"
+          id={id}
+          ref={ref}
+          onChange={() => onChange(data)}
+        />
         <div>
           <strong className="formTitle">{title}</strong>
           <p>{about}</p>
         </div>
       </div>
-      <p className="price">+${price}/mo</p>
+      <p className="price">
+        +${price}/{duration === "monthly" ? "mo" : "yr"}
+      </p>
     </label>
   );
-}
+});
+export default InputCheckBox;
