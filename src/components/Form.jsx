fd095e91d00bbf0dedee394a1e32660cf63de1c4
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-19 15:50:19 +0100

    implemented useRef

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,6 +6,7 @@ import FormSection4 from "./FormSection4";
 import Header from "./Header";
 import { header } from "../util/data";
 import SectionChange from "./SectionChangeBtn";
+import { useRef, useState } from "react";
 export default function Form({
   currentView,
   onNextView,
@@ -18,9 +19,41 @@ export default function Form({
   emailError,
   phoneError,
 }) {
+  const plans = useRef();
+  const duration = useRef();
+  const addOns = useRef();
+
+  const [plan, setplan] = useState("Advanced");
+  const [durations, setDurations] = useState("monthly");
+  // const [addOn, setAddOn] = useState([
+  //   {
+  //     name: "",
+  //     price: "",
+  //   },
+  // ]);
+
+  function handlePlan(newPlan) {
+    setplan(newPlan);
+  }
+  function handleDuration() {
+    if (duration.current.checked) {
+      setDurations("yearly");
+    } else {
+      setDurations("monthly");
+    }
+  }
+  // function handleAddOns(data) {
+  //   setAddOn((prev) => [
+  //     ...prev,
+  //     {
+  //       name: data.name,
+  //       price: data.price,
+  //     },
+  //   ]);
+  // }
   return (
     <>
-      <form action="#" method="post" onSubmit={() => onSubmit()}>
+      <form action="#" method="post" onSubmit={onSubmit}>
         <Header
           title={header[currentView].heading}
           paragraph={header[currentView].text}
@@ -35,9 +68,25 @@ export default function Form({
           emailError={emailError}
           phoneError={phoneError}
         />
-        <FormSection2 currentView={currentView} />
-        <FormSection3 currentView={currentView} />
-        <FormSection4 currentView={currentView} />
+        <FormSection2
+          currentView={currentView}
+          plans={plans}
+          handlePlan={handlePlan}
+          plan={plan}
+          ref={duration}
+          onChange={handleDuration}
+          checked={durations}
+        />
+        <FormSection3
+          currentView={currentView}
+          addOns={addOns}
+          // onChange={handleAddOns}
+        />
+        <FormSection4
+          currentView={currentView}
+          plan={plan}
+          duration={durations}
+        />
 
         <SectionChange
           onPreviousView={onPreviousView}
