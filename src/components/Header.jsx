0aa1034c6454c23416c2b00e2aec72b9ea1a77b9
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-23 02:34:51 +0100

    Updated the Header component with the use context method

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,14 @@
+import { useContext } from "react";
+import { Context } from "../App";
+import { header } from "../util/data";
 /* eslint-disable react/prop-types */
-export default function Header({ title, paragraph }) {
+export default function Header() {
+  const { view } = useContext(Context);
   return (
     <>
       <header>
-        <h1>{title}</h1>
-        <p>{paragraph}</p>
+        <h1>{header[view].heading}</h1>
+        <p>{header[view].text}</p>
       </header>
     </>
   );
