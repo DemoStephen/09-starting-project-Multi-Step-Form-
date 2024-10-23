e99dbe3fc57ab325c06ea0964df08bb7b3bcad25
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-10-23 02:46:44 +0100

    deleted irrelevant code

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,103 +1,24 @@
-/* eslint-disable react/prop-types */
+import { Context } from "../App";
 import FormSection1 from "./FormSection1";
 import FormSection2 from "./FormSection2";
 import FormSection3 from "./FormSection3";
 import FormSection4 from "./FormSection4";
 import Header from "./Header";
-import { header } from "../util/data";
 import SectionChange from "./SectionChangeBtn";
-import { useRef, useState } from "react";
-export default function Form({
-  currentView,
-  onNextView,
-  onPreviousView,
-  onSubmit,
-  name,
-  email,
-  phone,
-  nameError,
-  emailError,
-  phoneError,
-}) {
-  const plans = useRef();
-  const duration = useRef();
-  const addOn = useRef();
+import { useContext } from "react";
 
-  const [plan, setplan] = useState("Advanced");
-  const [durations, setDurations] = useState("monthly");
-  const [addOns, setAddOns] = useState([
-    {
-      name: "",
-      price: "",
-      abbr: "",
-      id: "",
-    },
-  ]);
+export default function Form() {
+  const { submit } = useContext(Context);
 
-  function handlePlan(newPlan) {
-    setplan(newPlan);
-  }
-  function handleDuration() {
-    if (duration.current.checked) {
-      setDurations("yearly");
-    } else {
-      setDurations("monthly");
-    }
-  }
-  function handleAddOns(data) {
-    setAddOns((prev) => [
-      ...prev,
-      {
-        name: data.name,
-        price: data.price,
-        abbr: data.abbr,
-        id: data.id,
-      },
-    ]);
-  }
   return (
     <>
-      <form action="#" method="post" onSubmit={onSubmit}>
-        <Header
-          title={header[currentView].heading}
-          paragraph={header[currentView].text}
-        />
-        <FormSection1
-          currentView={currentView}
-          name={name}
-          email={email}
-          phone={phone}
-          nameError={nameError}
-          emailError={emailError}
-          phoneError={phoneError}
-        />
-        <FormSection2
-          currentView={currentView}
-          plans={plans}
-          handlePlan={handlePlan}
-          plan={plan}
-          ref={duration}
-          onChange={handleDuration}
-          checked={durations}
-        />
-        <FormSection3
-          currentView={currentView}
-          onChange={handleAddOns}
-          AddOn={addOn}
-          duration={durations}
-        />
-        <FormSection4
-          currentView={currentView}
-          plan={plan}
-          duration={durations}
-          addOns={addOns}
-        />
-
-        <SectionChange
-          onPreviousView={onPreviousView}
-          onNextView={onNextView}
-          currentView={currentView}
-        />
+      <form action="#" method="post" onSubmit={submit}>
+        <Header />
+        <FormSection1 />
+        <FormSection2 />
+        <FormSection3 />
+        <FormSection4 />
+        <SectionChange />
       </form>
     </>
   );
