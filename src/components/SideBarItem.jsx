84322108b2857eaa10648f556cb0d8e79ad87a34
Adewale Stephen <adewalestephennifemi@gmail.com>
2024-12-08 18:17:32 +0100

    Sidebar Item can now change form section

--- src/components/SideBarItem.jsx
+++ src/components/SideBarItem.jsx
@@ -1,24 +1,18 @@
+/* eslint-disable no-unused-vars */
 import { useContext } from "react";
 import { Context } from "../App";
 /* eslint-disable react/prop-types */
 export default function SideBarItem({ title, step }) {
-  const { view } = useContext(Context);
-
-  // gap: 1rem;
-  // align-items: center;
-  // text-transform: uppercase;
-  // padding: 0.25rem 0.15rem;
-
-  // background-color: var(--lightblue);
-  // border: 2px solid var(--lightblue);
-  // color: var(--marineblue);
-
+  const { view, handleView } = useContext(Context);
 
   return (
     <>
       <section className="flex items-center uppercase md:gap-4">
         <div
-          className={`flex w-12 h-12 items-center justify-center rounded-full border-2 font-semibold ${view === step - 1 && "bg-slate-200 border-white text-blue-500"}`}
+          onClick={() => handleView((prev) => step - 1)}
+          className={`flex w-12 h-12 items-center justify-center rounded-full border-2 cursor-pointer font-semibold ${
+            view === step - 1 && "bg-slate-200 border-white text-blue-500"
+          }`}
         >
           {step}
         </div>
